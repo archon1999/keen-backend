7f4630ed18d38d461ccd6031e3ee80776cab977d
archon1999 <nazarbek.baltabaev@gmail.com>
2024-09-21 12:42:48 +0500

    [ADD] models | problems

--- apps/problems/admin.py
+++ apps/problems/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Attempt, Problem, ProblemCategory, ProblemSampleTest, ProblemAvailableLanguage, Category
+
+
+@admin.register(Attempt)
+class AttemptAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Problem)
+class ProblemAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProblemCategory)
+class ProblemCategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProblemSampleTest)
+class AProblemSampleTestAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProblemAvailableLanguage)
+class ProblemAvailableLanguageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
---
+++ apps/problems/models/__init__.py
@@ -0,0 +1,15 @@
+from .problem import Problem
+from .attempt import Attempt
+from .category import Category
+from .problem_available_language import ProblemAvailableLanguage
+from .problem_sample_test import ProblemSampleTest
+from .problem_category import ProblemCategory
+
+__all__ = [
+    'Attempt',
+    'Problem',
+    'Category',
+    'ProblemAvailableLanguage',
+    'ProblemSampleTest',
+    'ProblemCategory',
+]
