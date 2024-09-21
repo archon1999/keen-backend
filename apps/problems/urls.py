32f469b9fffc28101a027b633e8f672c5916b7c9
Olimboy <shavkatov.olimboy@mail.ru>
2024-09-22 00:41:13 +0500

    add submit

--- apps/problems/urls.py
+++ apps/problems/urls.py
@@ -1,3 +1,6 @@
+from django.urls import path, re_path
+
+from apps.problems.views.problem import ProblemSubmissionView
 from common.api.routers import OptionalSlashRouter
 
 from apps.problems.views import ProblemViewSet
@@ -5,4 +8,8 @@ from apps.problems.views import ProblemViewSet
 router = OptionalSlashRouter()
 router.register('problems', ProblemViewSet, basename='problems')
 
-urlpatterns = router.urls
+urlpatterns = [
+    re_path(r'problems/(?P<id>\d+)/submit/?$', ProblemSubmissionView.as_view(), name='problem-submit'),
+]
+
+urlpatterns += router.urls
