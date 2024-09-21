e8bb96be4c5b878cfb86b518a61593791adffe2d
Olimboy <shavkatov.olimboy@mail.ru>
2024-09-21 15:22:04 +0500

    fix admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,9 +1,51 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from apps.users.models import User
 
 
 @admin.register(User)
 class UserAdmin(UserAdmin):
-    pass
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("role", "first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    # "groups",
+                    # "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2"),
+            },
+        ),
+        (_("Personal info"), {"fields": ("role", "first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    # "groups",
+                    # "user_permissions",
+                ),
+            },
+        ),
+    )
+
+    list_display = ("username", "first_name", "last_name", "role")
+    list_filter = ("role", "is_superuser", "is_active")
