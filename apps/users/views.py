64bd4b1e9315fc821564bf6327623b642ed15e3a
archon1999 <nazarbek.baltabaev@gmail.com>
2024-09-21 12:21:33 +0500

    [ADD] auth

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.contrib.auth import login as django_login
+from django.contrib.auth import logout as django_logout
+from django.utils import translation
+from rest_framework.authentication import (SessionAuthentication,
+                                           TokenAuthentication)
+from rest_framework.decorators import (
+    api_view,
+    authentication_classes,
+    permission_classes,
+)
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-# Create your views here.
+from apps.users.serializers import UserAuthSerializer
+from core import settings
+
+
+@api_view(["POST"])
+@authentication_classes([SessionAuthentication,
+                         TokenAuthentication])
+def login(request: Request) -> Response:
+    django_login(request, request.user)
+    return Response(UserAuthSerializer(request.user).data)
+
+
+@api_view(["POST"])
+@authentication_classes([SessionAuthentication])
+@permission_classes([IsAuthenticated])
+def logout(request: Request) -> Response:
+    django_logout(request)
+    return Response({})
+
+
+@api_view(["GET"])
+@authentication_classes([SessionAuthentication,
+                         TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def me(request: Request) -> Response:
+    user = request.user
+    return Response(UserAuthSerializer(user).data)
+
+
+@api_view(["POST"])
+def set_language(request: Request) -> Response:
+    language = request.data['language']
+    translation.activate(language)
+    response = Response({})
+    response.set_cookie(settings.KEENSettings.LANGUAGE_COOKIE_NAME,
+                        language)
+    return response
