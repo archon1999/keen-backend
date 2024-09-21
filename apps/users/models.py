bcc4d900962954f3bc6b0726a4a8ae1924fe2420
archon1999 <nazarbek.baltabaev@gmail.com>
2024-09-21 12:21:23 +0500

    [ADD] storage & avatar field to user

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from common.storages import UserAvatarStorage
+
 
 class User(AbstractUser):
     keencoin = models.IntegerField(default=0)
+    avatar = models.ImageField(
+        default='default_standart.webp',
+        storage=UserAvatarStorage
+    )
